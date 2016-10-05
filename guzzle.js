59ca2874adb60c25ac72f53b271536d34a3ed46a
kurian <kurianck.mail@gmail.com>
2016-10-05 05:57:33 +0530

    UPDATE
    observation:adding gulp
    suggestion:gulp is added to automate code

--- guzzle.js
+++ guzzle.js
@@ -1,1 +1,12 @@
-require('guzzle');
+var gulp = require('gulp');
+
+require('gulp-concat');
+require('gulp-uglify');
+require('gulp-jshint');
+
+gulp.task('default', function() {
+  gulp.src('./src/*.js')
+    .pipe(jshint())
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist'));
+});
